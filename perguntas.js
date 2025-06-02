cb5651d6d97e6f5dcc691a7b1d981505a46c4aa0
Guilherme80242 <guilherme.chechi@escola.pr.gov.br>
2025-06-02 08:54:58 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Enigma',
+    'Meu trovão vem antes do relâmpago, meu raio vem antes das nuvens e minha chuva seca toda terra em que toca. Quem eu sou??',
+    'O vulcão.'
+)
+
+criaCartao(
+    'Entretenimento',
+    'Qual o personagem principal da série the boys?',
+    'Capitão Pátria'
+)
+
+criaCartao(
+    'Futebol',
+    'Quando foi fundado o Corinthians?',
+    'Em 1 de setembro de 1910'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual melhor jogador do mundo?',
+    'Cristiano Ronaldo'
+)
